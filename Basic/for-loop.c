0f1c15d3287fd84c442af82e3c08da2f49467615
Souman <soumanroy.gsa@gmail.com>
2017-04-25 11:06:03 +0530

    IF-Statement

---
+++ Basic/for-loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 4;
+	int b = 5;
+	int c = 6;
+
+	if (a==b){
+	    printf("This won't happen.\n");
+        }else if (a==4){
+	    printf("yes this can be true.\n");
+        }else{
+	printf("Bye Bye");
+ }
+	return 0;
+
+}
+
