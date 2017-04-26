a6ba4528edbfcf287b6e7d905a240bb1629b7859
Souman <soumanroy.gsa@gmail.com>
2017-04-26 22:47:08 +0530

    while-loop

---
+++ Basic/while2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* while loop for reverse countdown*/
+
+int main(){
+        int a  = 21;      /*set your value*/
+
+	while(a >= 3) {   /* while loop exectution */
+
+		printf("Countdown starts %d\n",a); 
+		a--;    /* decrement operator */
+	}
+		return 0;
+}
+
