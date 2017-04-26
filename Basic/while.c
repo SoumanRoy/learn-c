cbb0189bd213d7356dc291fa7388961703c2a602
Souman <soumanroy.gsa@gmail.com>
2017-04-26 13:01:57 +0530

    while

---
+++ Basic/while.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+
+	int a = 5;            /* local variable definition */                  
+	int b = 7;
+
+	while (a<=b){                            /* while loop execution */
+		printf("This will execute.\n"); 
+	break;
+  }
+	return 0;
+}
+
+
